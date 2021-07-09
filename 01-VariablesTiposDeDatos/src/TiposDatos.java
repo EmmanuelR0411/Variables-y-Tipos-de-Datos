079e2b1118e08e9ae1391f14b4361996def4dbdf
Emmanuel <emmanuelreyna0410@gmail.com>
2021-07-09 15:08:35 -0500

    Conociendo las variables y tipos de datos basicos en java

---
+++ 01-VariablesTiposDeDatos/src/TiposDatos.java
@@ -0,0 +1,33 @@
+
+public class TiposDatos {
+
+//	Dentro del metodo main se escribe todo el codigo a ejecutar, es el punto
+//	de entrada hacia la aplicacion, solo debe dehaber un metodo main
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//	System.out.println(); es para imprimir en pantalla	
+		System.out.println("Hola mundo");
+
+//	Numeros enteros, declaracion
+		int numero1 = 30;
+		int numero2 = 8;
+//	Valores booleanos
+		boolean mayor18 = true;
+		
+//	Cadenas de texto
+		String cadena1 = "Texto 1";
+		String cadena2 = "Texto 2";
+		
+		String oracion = cadena1 + cadena2;
+		
+		System.out.println(oracion);
+		
+		int multiplicacion = numero1 * numero2;
+		
+		System.out.println(multiplicacion);
+		
+		
+	}
+
+}
